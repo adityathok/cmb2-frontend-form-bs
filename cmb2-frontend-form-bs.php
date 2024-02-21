1d1d1a7593532e9f34f43cd2794f6b80ff27da10
Aditya Thok <45733361+adityathok@users.noreply.github.com>
2024-02-21 11:40:32 +0700

    Update cmb2-frontend-form-bs.php

--- cmb2-frontend-form-bs.php
+++ cmb2-frontend-form-bs.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * CMB2 Front-end Shortcode Bootstrap 5 
+ *
+ * Author: Adityathok
+ * Version: 1.0.0
+ * Author URI: https://github.com/adityathok
+ * GitHub URI: https://github.com/adityathok/cmb2-frontend-form-bs
+*/
+
+/**
+ * Generate the frontend form shortcode for CMB2.
+ * 
+ * [cmb-form id="id_metabox" post_id=94]
+ * 
+ * @param array $atts Shortcode attributes.
+ * @return string The output of the frontend form shortcode.
+ */
 add_shortcode( 'cmb-form', 'cmb2_do_frontend_form_shortcode' );
 function cmb2_do_frontend_form_shortcode( $atts = array() ) {
 
@@ -34,22 +51,43 @@ function cmb2_do_frontend_form_shortcode( $atts = array() ) {
         if ( is_wp_error( $new_id ) ) {
 
             // If there was an error with the submission, add it to our ouput.
-            $output .= '<div class="alert alert-warning">' . sprintf( __( 'There was an error in the submission: %s', 'wds-post-submit' ), '<strong>'. $new_id->get_error_message() .'</strong>' ) . '</div>';
+            $output .= '<div class="alert alert-warning">' . sprintf( __( 'There was an error in the submission: %s', 'cmb2-post-submit' ), '<strong>'. $new_id->get_error_message() .'</strong>' ) . '</div>';
 
         } else {
 
             // Add notice of submission
-            $output .= '<div class="alert alert-success">' . sprintf( __( 'Produk %s, berhasil di submit.', 'wds-post-submit' ), esc_html( get_the_title($new_id) ) ) . '</div>';
+            $output .= '<div class="alert alert-success">' . sprintf( __( 'Produk %s, berhasil di submit.', 'cmb2-post-submit' ), esc_html( get_the_title($new_id) ) ) . '</div>';
         }
 
     }
 
     // Get our form
-    $form = cmb2_get_metabox_form( $cmb, $object_id, array( 'save_button' => __( 'Submit Produk', 'wds-post-submit' ) ) );
-    $form = str_replace('regular-text', 'regular-text form-control"', $form);
-    $form = str_replace('class="cmb-th"', 'class="cmb-th w-100 p-0"', $form);
-    $form = str_replace('class="cmb-td"', 'class="cmb-td w-100 p-0 pb-2"', $form);
-    $form = str_replace('class="button-primary"', 'class="button-primary btn btn-primary"', $form);
+    $form = cmb2_get_metabox_form( $cmb, $object_id, array( 'save_button' => __( 'Submit Produk', 'cmb2-post-submit' ) ) );
+    
+    // Format our form use Bootstrap 5
+    $styling = [
+        'regular-text'              => 'regular-text form-control',
+        'cmb2-text-small'           => 'cmb2-text-small form-control',
+        'cmb2-text-medium'          => 'cmb2-text-medium form-control',
+        'cmb2-timepicker'           => 'cmb2-timepicker form-control d-inline-block',
+        'cmb2-datepicker'           => 'cmb2-datepicker d-inline-block',
+        'cmb2-text-money'           => 'cmb2-text-money form-control d-inline-block',
+        'cmb2_textarea'             => 'cmb2_textarea form-control',
+        'cmb2-textarea-small'       => 'cmb2-textarea-small form-control d-inline-block',
+        'cmb2_select'               => 'cmb2_select form-select',
+        'cmb2-upload-file regular-text'         => 'cmb2-upload-file regular-text d-inline-block',
+        'type="radio" class="cmb2-option"'      => 'type="radio" class="cmb2-option form-check-input"',
+        'type="checkbox" class="cmb2-option"'   => 'type="checkbox" class="cmb2-option form-check-input"',
+        'button-secondary'          => 'button-secondary btn-sm btn btn-outline-secondary',
+        'button-secondary btn-sm btn btn-outline-secondary cmb-remove-row-button'   => 'button-secondary btn btn-danger cmb-remove-row-button',
+        'class="button-primary"'    => 'class="button-primary btn btn-primary"',
+        'class="cmb-th"'            => 'class="cmb-th w-100 p-0"',
+        'class="cmb-td"'            => 'class="cmb-th w-100 p-0 pb-2"',
+        'class="cmb-add-row"'       => 'class="cmb-add-row text-end"',
+    ];
+    foreach ($styling as $std => $newf) {
+        $form = str_replace($std, $newf, $form);
+    }
 
     $output .= $form;
 
@@ -59,7 +97,7 @@ function cmb2_do_frontend_form_shortcode( $atts = array() ) {
 /**
  * Handles form submission on save
  *
- * @param  CMB2  $cmb The CMB2 object
+ * @param  CMB2  $cmb       The CMB2 object
  * @param  array $post_data Array of post-data for new post
  * @return mixed New post ID if successful
  */
